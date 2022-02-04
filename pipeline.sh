242c60f61bdeac830c42ee02e6b130cf019439ed
Simon <simondonaldmurray@gmail.com>
2022-02-04 12:13:06 +0000

    made default input BAM and updated filtering

--- pipeline.sh
+++ pipeline.sh
@@ -6,13 +6,13 @@ input_file=""
 output_file="out.bam"
 input_vcf=""
 input_fasta=""
-input_sam=true
-input_bam=false
+input_sam=false
+input_bam=true
 input_cram=false
 remove_chr=true
 remove_tmp=true
 
-while getopts :i:o:v:f:bcrkh opt
+while getopts :i:o:v:f:scrkh opt
 do
     case "$opt" in
     i)
@@ -27,9 +27,9 @@ do
     f)
       input_fasta=$OPTARG
       ;;
-    b)
-      input_sam=false
-      input_bam=true
+    s)
+      input_sam=true
+      input_bam=false
       input_cram=false
       ;; 
     c)
@@ -49,7 +49,7 @@ do
 -o path to output location (default CWD)
 -v path to input vcf for chrom comparison
 -f path to reference fasta file (needed with CRAM)
--b input file type is bam
+-s input file type is sam
 -c input file type is cram
 -r add "chr" to file (default is to remove)
 -k does not remove temporary bam files (default is to remove)
@@ -79,32 +79,32 @@ if $input_cram && [[ $input_fasta == "" ]]; then
    false
 fi
 
-#convert BAM to SAM
-if $input_bam; then
-  echo "input is BAM file"
-  samtools view -h -o tmp.sam $input_file
-  input_file=tmp.sam
+#convert SAM to BAM
+if $input_sam; then
+  echo "input is SAM file"
+  samtools view -b -o tmp.bam $input_file
+  input_file=tmp.bam
 fi
 
 #conver CRAM to SAM
 if $input_cram; then
   echo "input is CRAM file (default)"
-  samtools view -h -T $input_fasta -o tmp.sam $input_file
-  input_file=tmp.sam
+  samtools view -b -T $input_fasta -o tmp.bam $input_file
+  input_file=tmp.bam
 fi
 
-if $input_sam; then
-  echo "input is SAM file (default)"
+if $input_bam; then
+  echo "input is BAM file (default)"
 fi
 
 # get chromosome nomenclature of vcf and input file
 grep -v '#' $input_vcf | cut -f 1 | uniq > vcf_chroms.txt
-samtools view $input_file | cut -f 3 | uniq > input_chroms.txt
+samtools view -H $input_file | grep @SQ | cut -f 2 | cut -c 4- > input_chroms.txt
 
 # reorder header for sorting later and so chromosome order goes 1, 2, 3 not chr1, chr10, chr11
 if $remove_chr; then
-	grep "@HD" $input_file > filtered.sam
-	grep "@SQ" $input_file | grep "chr*" > sq.tmp
+  samtools view -H $input_file | grep @HD > filtered.sam
+  samtools view -H $input_file | grep @SQ | grep "chr*" > sq.tmp
 	> reorder.tmp
 	> store.tmp
 	cat sq.tmp | while read i; do
@@ -123,17 +123,15 @@ if $remove_chr; then
 	cat tab.tmp >> filtered.sam
 	rm reorder.tmp
 	rm tab.tmp
-	grep "@PG" $input_file >> filtered.sam
-	grep "@CO" $input_file >> filtered.sam
+  samtools view -H $input_file | grep @PG >> filtered.sam
+  samtools view -H $input_file | grep @CO >> filtered.sam
 fi
 
 # remove any contigs present in input file that are not named chr
 if $remove_chr; then
-  cat input_chroms.txt | while read i; do
-    if [[ $i == chr* ]]; then
-      grep -v "@" $input_file | grep $i >> filtered.sam
-    fi;
-  done
+  samtools index $input_file
+  chroms=$(grep "chr" input_chroms.txt)
+  samtools view $input_file $chroms >> filtered.sam 
 else
   cat $input_file > filtered.sam
 fi
@@ -143,23 +141,28 @@ if $remove_chr; then
   sed s/chr/""/g filtered.sam > altered.sam
 else
 	samtools view -H filtered.sam | grep "@HD" > altered.sam
-	samtools view -H filtered.sam | grep "@SQ" | sed -r -e 's/^.{7}/&chr/' >> altered.sam
+  samtools view -H filtered.sam | grep "@SQ" | sed -r -e 's/^.{7}/&chr/' >> altered.sam
 	samtools view -H filtered.sam | grep "@PG" >> altered.sam
 	samtools view -H filtered.sam | grep "@CO" >> altered.sam
 	samtools view filtered.sam | awk '$3="chr"$3' | sed -e 's/  */\t/g' >> altered.sam
 fi
 
-# sorting BAM file based on new chromosome order
+# sorting SAM file based on new chromosome order
 if $remove_chr; then
   samtools sort -o sorted.sam altered.sam
 fi
 
+# convertin SAM to BAM for output
+samtools view -b -o out.bam sorted.sam
+
 # removing temporary files
 if $remove_tmp; then
-  if [[ $input_sam == false ]]; then
-    rm tmp.sam
+  if [[ $input_bam == false ]]; then
+    rm tmp.bam
   fi
   rm filtered.sam
+  rm sorted.sam
+  rm $input_file.bai
   if $remove_chr; then
     rm altered.sam
   fi
