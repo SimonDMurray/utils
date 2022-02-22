85d418b635a80d0db7b639cb69e21c5b92df7ee7
Simon <simondonaldmurray@gmail.com>
2022-02-22 16:15:34 +0000

    changed script that generates barcode rank plot

--- barcode-rank-plot.R
+++ barcode-rank-plot.R
@@ -1,27 +1,10 @@
-
 # This script creates a basic barcode rank plot similar to that seen in cellranger except using Starsolo ouput
-
-## The list of UMIs provided by starsolo in the output file UMIperCellSorted.txt is plotted on the y axis with the row number plotted on the x axis
-
-## Both axes are log scalled
-
-## You can customise plot to make it look prettier with colours, annotations and change axis cutoffs for customisation
-
-```{r}
+# The list of UMIs provided by starsolo in the output file UMIperCellSorted.txt is plotted on the y axis with the row number plotted on the x axis
+# Both axes are log scalled
+# You can customise plot with colours, annotations and change axis cutoffs etc
 library(ggplot2)
-```
-
-```{r}
 umi <- read.table("/lustre/scratch117/cellgen/cellgeni/TIC-starsolo/tic-1313/results/FCA_GND8047885/output/GeneFull/UMIperCellSorted.txt")
 umi$row_num <- seq.int(nrow(umi))
-```
-
-```{r}
 p <- ggplot(umi, aes(x = umi$row_num, y = umi$V1)) + geom_point() + scale_x_continuous(trans = 'log10') +
   scale_y_continuous(trans = 'log10') + xlim(0, 15000) + ylim(0, 15000)
-```
-
-```{r}
 p
-```
-
